a4601bc3d4620a7576468480f9afa9075f37ec56
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2025-07-15 20:47:34 +0200

    Create project

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// this is for emacs file handling -*- mode: c++; indent-tabs-mode: nil -*-
+
+// -- BEGIN LICENSE BLOCK ----------------------------------------------
+// -- END LICENSE BLOCK ------------------------------------------------
+
+//----------------------------------------------------------------------
+/*!\file
+ *
+ * \author  Robert Wilbrandt <robert@stamm-wilbrandt.de>
+ * \date    2025-07-15
+ *
+ */
+//----------------------------------------------------------------------
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "Hello World!" << std::endl;
+  return EXIT_SUCCESS;
+}
