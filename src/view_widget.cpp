5735b1ef43e1e9124495a6088ba2b399637fbacd
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2025-07-16 19:10:06 +0200

    Define surface format

--- src/view_widget.cpp
+++ src/view_widget.cpp
@@ -13,11 +13,19 @@
 //----------------------------------------------------------------------
 #include "cad_viewer/view_widget.h"
 
+#include <QSurfaceFormat>
+
 namespace cad_viewer {
 
 ViewWidget::ViewWidget(QWidget* parent)
   : QOpenGLWidget{parent}
 {
+  // Set surface format
+  QSurfaceFormat format{};
+  format.setDepthBufferSize(24);
+  format.setStencilBufferSize(8);
+  format.setProfile(QSurfaceFormat::CompatibilityProfile);
+  setFormat(format);
 }
 
 } // namespace cad_viewer
